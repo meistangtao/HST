4d332038a4f4b9bfba3138520d49edcf4bd88fa3
Tang Tao <meistangtao@163.com>
2021-06-22 17:41:47 +0800

    项目结构调整

---
+++ gulimal-pom/src/main/java/com/tangtao/gulimall/pom/GulimalPomApplication.java
@@ -0,0 +1,13 @@
+package com.tangtao.gulimall.pom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimalPomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimalPomApplication.class, args);
+    }
+
+}
---
+++ gulimal-pom/src/test/java/com/tangtao/gulimall/pom/GulimalPomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tangtao.gulimall.pom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimalPomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
